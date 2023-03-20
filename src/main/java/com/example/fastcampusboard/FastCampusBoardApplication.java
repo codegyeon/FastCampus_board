db93071ab047d058a12fb1de30446e7078e87ce9
codegyeon <wjdrlgus313@naver.com>
2023-03-20 19:01:12 +0900

    #1: setting

---
+++ src/main/java/com/example/fastcampusboard/FastCampusBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastcampusboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCampusBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fastcampusboard/FastCampusBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastcampusboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
