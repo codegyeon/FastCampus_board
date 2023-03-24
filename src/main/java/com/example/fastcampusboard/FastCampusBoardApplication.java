05573839577ef0084d6d18da6195c6aeeca6bcc9
codegyeon <wjdrlgus313@naver.com>
2023-03-24 19:28:54 +0900

    #5 게시글 페이지 및 컨트롤러 설정, thymeleaf 설정

--- src/main/java/com/example/fastcampusboard/FastCampusBoardApplication.java
+++ src/main/java/com/example/fastcampusboard/FastCampusBoardApplication.java
@@ -2,7 +2,9 @@ package com.example.fastcampusboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastCampusBoardApplication {
 
